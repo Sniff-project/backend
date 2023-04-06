5e565652120b8acd4c344dff379039d6ad2b994a
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-06 16:08:38 +0300

    Setup project

---
+++ src/main/java/com/sniff/SniffApplication.java
@@ -0,0 +1,13 @@
+package com.sniff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SniffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SniffApplication.class, args);
+    }
+
+}
