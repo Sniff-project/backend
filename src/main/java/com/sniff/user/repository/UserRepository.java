1e5b5a965aac59d94c8fcd196f8b36b86091760e
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-20 22:18:23 +0300

    Create UserRepository

---
+++ src/main/java/com/sniff/user/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.sniff.user.repository;
+
+import com.sniff.user.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByEmailIgnoreCase(String email);
+
+    Boolean existsByPhone(String phone);
+
+    Optional<User> findByEmail(String email);
+}
