8e866849c1fce2920c78f6c8dffc55b94be0d5ef
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-20 22:19:16 +0300

    Create HttpResponse dto

---
+++ src/main/java/com/sniff/utils/HttpResponse.java
@@ -0,0 +1,12 @@
+package com.sniff.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class HttpResponse {
+    private String message;
+}
