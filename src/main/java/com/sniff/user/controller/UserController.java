7231bf78fc8c1a4b4ba54a3015038b4793d57c66
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-21 19:30:05 +0300

    Create UserController and add endpoint for getting user profile, and add swagger documentation

---
+++ src/main/java/com/sniff/user/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.sniff.user.controller;
+
+import com.sniff.user.model.response.UserFullProfile;
+import com.sniff.user.model.response.UserProfile;
+import com.sniff.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+@Tag(name = "User", description = "User APIs documentation")
+public class UserController {
+    private final UserService userService;
+
+    @SecurityRequirement(name = "bearerAuth")
+    @Operation(
+            summary = "Get user profile",
+            description = "As a user, I want to be able to view my profile information, " +
+                    "as well as the profile information of other users."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",
+                    content = @Content(mediaType = "application/json",
+                                    schema = @Schema(oneOf = {
+                                            UserProfile.class,
+                                            UserFullProfile.class
+                                    }))),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content)
+    })
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserProfile getUserProfile(@PathVariable Long id) {
+        return userService.getUserProfileById(id);
+    }
+}
