2fb5af35f462210caa2d35be5e58b2b62802cb49
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-21 19:27:13 +0300

    Remove builder annotation from UserSignIn

--- src/main/java/com/sniff/user/model/request/UserSignIn.java
+++ src/main/java/com/sniff/user/model/request/UserSignIn.java
@@ -4,14 +4,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@Builder
 public class UserSignIn {
     @Email(message = "Email should be valid")
     @NotBlank(message = "Email shouldn't be blank")
