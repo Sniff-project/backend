a074bdad6731a60df496b44d63fd8d1f680a42f4
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-22 23:51:44 +0300

    Optimize jwt token validation in JwtValidationFilter

--- src/main/java/com/sniff/jwt/JwtValidationFilter.java
+++ src/main/java/com/sniff/jwt/JwtValidationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -22,36 +23,25 @@ public class JwtValidationFilter extends OncePerRequestFilter {
     private final ObjectMapper objectMapper;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
         String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         if (authorizationHeader != null && authorizationHeader.startsWith(BEARER_PREFIX)) {
             String token = authorizationHeader.substring(BEARER_PREFIX.length());
 
             if(!jwtService.isTokenValid(token)) {
-                returnResponse(response, HttpStatus.FORBIDDEN, FORBIDDEN_MESSAGE);
-                return;
-            }
-
-            if (jwtService.isTokenExpired(token)) {
-                returnResponse(response, HttpStatus.UNAUTHORIZED, EXPIRED_TOKEN_MESSAGE);
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                String responseBody = objectMapper.writeValueAsString(new HttpResponse(INVALID_TOKEN_MESSAGE));
+                response.getWriter().write(responseBody);
                 return;
             }
         }
 
         filterChain.doFilter(request, response);
-    }
-
-    private void returnResponse(HttpServletResponse response,
-                                HttpStatus status,
-                                String message) throws IOException {
-        response.setStatus(status.value());
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-
-        String responseBody = objectMapper.writeValueAsString(new HttpResponse(message));
-        response.getWriter().write(responseBody);
     }
 }
