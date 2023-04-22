89a9d496fd827fc886b45cfe3ba29534f8ce5a96
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-22 23:52:07 +0300

    Add new constant to JwtConstants

--- src/main/java/com/sniff/jwt/JwtConstants.java
+++ src/main/java/com/sniff/jwt/JwtConstants.java
@@ -3,7 +3,7 @@ package com.sniff.jwt;
 public class JwtConstants {
     public static final String BEARER_PREFIX = "Bearer ";
     public static final long EXPIRATION_TIME = 60; // 60 minutes
-    public static final String EXPIRED_TOKEN_MESSAGE = "Token has expired";
+    public static final String INVALID_TOKEN_MESSAGE = "Invalid token";
     public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
     public static final String FORBIDDEN_MESSAGE = "You need to sign in to access this page";
     public static final String TOKEN_ISSUER = "Sniff";
