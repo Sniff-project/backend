9c9d98dad1c6d81d0622784ecd9179643de2f776
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-30 20:14:18 +0300

    Add new methods to AuthVerifyService

--- src/main/java/com/sniff/auth/service/AuthVerifyService.java
+++ src/main/java/com/sniff/auth/service/AuthVerifyService.java
@@ -1,8 +1,11 @@
 package com.sniff.auth.service;
 
+import com.sniff.auth.exception.DeniedAccessException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class AuthVerifyService {
@@ -10,10 +13,19 @@ public class AuthVerifyService {
         return getIdFromSubject() != 0L;
     }
 
-    public long getIdFromSubject() {
+    public Long getIdFromSubject() {
         if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof Jwt jwt) {
             return Long.parseLong(jwt.getSubject());
         }
         return 0L;
+    }
+
+    public void verifyAccess(Long requiredId) {
+        if (!isPersonalProfile(requiredId))
+            throw new DeniedAccessException("You have no access to this resource");
+    }
+
+    public boolean isPersonalProfile(Long requiredId){
+        return Objects.equals(getIdFromSubject(), requiredId);
     }
 }
