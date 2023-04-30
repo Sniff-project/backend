ec0e3123d9fc4b2bf38454fc2ac2e5999bf087fd
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-30 20:15:49 +0300

    Add new method for updating user profile in UserService

--- src/main/java/com/sniff/user/service/UserService.java
+++ src/main/java/com/sniff/user/service/UserService.java
@@ -2,17 +2,24 @@ package com.sniff.user.service;
 
 import com.sniff.auth.service.AuthVerifyService;
 import com.sniff.mapper.UserMapper;
+import com.sniff.user.exception.InvalidPhoneException;
+import com.sniff.user.exception.UserExistsException;
 import com.sniff.user.exception.UserNotFoundException;
 import com.sniff.user.model.entity.User;
+import com.sniff.user.model.request.UserUpdate;
 import com.sniff.user.model.response.UserProfile;
 import com.sniff.user.repository.UserRepository;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+import static com.sniff.utils.Validation.isValidPhone;
+
 @Service
-@RequiredArgsConstructor
 @Transactional
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
@@ -24,6 +31,43 @@ public class UserService {
         return authVerifyService.isAuthenticated()
                 ? userMapper.toUserFullProfile(user)
                 : userMapper.toUserProfile(user);
+    }
+
+    public UserProfile updateUserProfile(Long id, UserUpdate updatedUser) {
+        User userToUpdate = getUserById(id);
+
+        authVerifyService.verifyAccess(id);
+
+        if(!userToUpdate.getEmail().equals(updatedUser.getEmail())) {
+            if(userRepository.existsByEmailIgnoreCase(updatedUser.getEmail())) {
+                throw new UserExistsException("User with email " + updatedUser.getEmail() + " already exists");
+            }
+            userToUpdate.setEmail(updatedUser.getEmail());
+        }
+
+        if(!userToUpdate.getPhone().equals(updatedUser.getPhone())) {
+            if(!isValidPhone(updatedUser.getPhone())) {
+                throw new InvalidPhoneException("Invalid phone number");
+            }
+
+            if(userRepository.existsByPhone(updatedUser.getPhone())) {
+                throw new UserExistsException("User with phone " + updatedUser.getPhone() + " already exists");
+            }
+            userToUpdate.setPhone(updatedUser.getPhone());
+        }
+
+        userToUpdate.setFirstname(updatedUser.getFirstname());
+        userToUpdate.setLastname(updatedUser.getLastname());
+
+        Optional<String> optionalRegion = Optional.ofNullable(updatedUser.getRegion());
+        optionalRegion.ifPresent(userToUpdate::setRegion);
+
+        Optional<String> optionalCity = Optional.ofNullable(updatedUser.getCity());
+        optionalCity.ifPresent(userToUpdate::setCity);
+
+        userRepository.save(userToUpdate);
+
+        return userMapper.toUserFullProfile(userToUpdate);
     }
 
     private User getUserById(Long id) {
