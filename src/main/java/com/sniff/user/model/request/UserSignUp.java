30393f8f0c233a69b7a6f513812698122cfac024
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-30 20:13:59 +0300

    Update password length in models

--- src/main/java/com/sniff/user/model/request/UserSignUp.java
+++ src/main/java/com/sniff/user/model/request/UserSignUp.java
@@ -31,6 +31,6 @@ public class UserSignUp {
     private String phone;
 
     @NotBlank(message = "Password shouldn't be blank")
-    @Size(min = 6, max = 20, message = "Password should be between 6 and 20 characters")
+    @Size(min = 8, max = 20, message = "Password should be between 8 and 20 characters")
     private String password;
 }
