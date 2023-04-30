3abb1848bf81108ce195a6b829cba0e284ce83d0
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-04-30 20:33:27 +0300

    Add tests for updating user profile in UserControllerTest

--- src/test/java/com/sniff/user/UserControllerTest.java
+++ src/test/java/com/sniff/user/UserControllerTest.java
@@ -1,11 +1,13 @@
 package com.sniff.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sniff.auth.exception.DeniedAccessException;
 import com.sniff.auth.service.AuthVerifyService;
 import com.sniff.jwt.JwtService;
 import com.sniff.user.controller.UserController;
 import com.sniff.user.exception.UserNotFoundException;
 import com.sniff.user.model.entity.User;
+import com.sniff.user.model.request.UserUpdate;
 import com.sniff.user.model.response.UserFullProfile;
 import com.sniff.user.model.response.UserProfile;
 import com.sniff.user.service.UserService;
@@ -24,8 +26,10 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -112,5 +116,80 @@ public class UserControllerTest {
         response
                 .andDo(print())
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("[Sprint-2] Update own profile successfully")
+    public void updateOwnProfileSuccessfully() throws Exception {
+        UserUpdate updateRequest = generateUpdateRequest();
+        UserFullProfile userFullProfile = new UserFullProfile(
+                user.getAvatar(),
+                updateRequest.getFirstname(),
+                updateRequest.getLastname(),
+                updateRequest.getRegion(),
+                updateRequest.getCity(),
+                updateRequest.getEmail(),
+                updateRequest.getPhone());
+        given(authVerifyService.isPersonalProfile(anyLong())).willReturn(true);
+        given(userService.updateUserProfile(anyLong(), any(UserUpdate.class))).willReturn(userFullProfile);
+
+        ResultActions response = mockMvc
+                .perform(MockMvcRequestBuilders.patch("/api/v1/users/{id}", user.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest))
+                        .accept(MediaType.APPLICATION_JSON));
+
+        response
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").exists());
+    }
+
+    @Test
+    @DisplayName("[Sprint-2] Try update someone else profile")
+    public void updateSomeoneElseProfile() throws Exception {
+        UserUpdate updateRequest = generateUpdateRequest();
+       given(userService.updateUserProfile(anyLong(), any(UserUpdate.class)))
+               .willThrow(new DeniedAccessException("You have no access to this resource"));
+
+        ResultActions response = mockMvc
+                .perform(MockMvcRequestBuilders.patch("/api/v1/users/{id}", user.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequest))
+                        .accept(MediaType.APPLICATION_JSON));
+
+        response
+                .andDo(print())
+                .andExpect(status().isForbidden())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").exists());
+    }
+
+    @Test
+    @DisplayName("[Sprint-2] Try to update non-existent user profile")
+    public void updateNonExistentUserProfile() throws Exception {
+        given(userService.updateUserProfile(anyLong(), any(UserUpdate.class)))
+                .willThrow(new UserNotFoundException("User not found"));
+
+        ResultActions response = mockMvc
+                .perform(MockMvcRequestBuilders.patch("/api/v1/users/{id}", user.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(generateUpdateRequest()))
+                        .accept(MediaType.APPLICATION_JSON));
+
+        response
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").exists());
+    }
+
+    private UserUpdate generateUpdateRequest(){
+        return UserUpdate.builder()
+                .firstname("John")
+                .lastname("Doe")
+                .email("johndoe@example.com")
+                .phone("+380111111111")
+                .region("Ukraine")
+                .city("Kiev")
+                .build();
     }
 }
