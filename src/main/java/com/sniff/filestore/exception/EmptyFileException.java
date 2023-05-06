896fa806325ee0770b21e8ce9a01d3a4330a10f7
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-05-06 18:57:07 +0300

    Add new FileStore exceptions

---
+++ src/main/java/com/sniff/filestore/exception/EmptyFileException.java
@@ -0,0 +1,7 @@
+package com.sniff.filestore.exception;
+
+public class EmptyFileException extends FileStoreException {
+    public EmptyFileException(String message) {
+        super(message);
+    }
+}
