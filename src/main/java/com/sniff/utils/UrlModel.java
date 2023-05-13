cfe822e706d0fcefd8e79335088bba52c8add3ac
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-05-13 18:56:02 +0300

    Rename UrlResponse to UrlModel

--- src/main/java/com/sniff/utils/UrlModel.java
+++ src/main/java/com/sniff/utils/UrlModel.java
@@ -1,7 +1,9 @@
 package com.sniff.utils;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
-public class UrlResponse {
+@NoArgsConstructor
+public class UrlModel {
+    @NotBlank(message = "Url can't be empty")
     private List<String> urls;
 }
