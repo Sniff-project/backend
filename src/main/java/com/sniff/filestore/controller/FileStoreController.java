86e19bea9659ca5248df122053e1c40aa8c5a564
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-05-13 17:29:17 +0300

    Update FileStoreController

--- src/main/java/com/sniff/filestore/controller/FileStoreController.java
+++ src/main/java/com/sniff/filestore/controller/FileStoreController.java
@@ -1,5 +1,6 @@
 package com.sniff.filestore.controller;
 
+import com.sniff.filestore.enums.FileStoreOperation;
 import com.sniff.filestore.service.FileStoreService;
 import com.sniff.utils.HttpResponse;
 import com.sniff.utils.UrlResponse;
@@ -17,8 +18,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,6 +35,7 @@ import org.springframework.web.multipart.MultipartFile;
         type = SecuritySchemeType.HTTP,
         in = SecuritySchemeIn.HEADER
 )
+@Validated
 public class FileStoreController {
     private final FileStoreService fileStoreService;
 
@@ -43,15 +48,35 @@ public class FileStoreController {
             @ApiResponse(responseCode = "400", description = "Invalid request",
                     content = { @Content(schema = @Schema(implementation = HttpResponse.class)) }) })
     @PostMapping(
-            path = "/{id}/upload",
+            path = "users/{id}/upload",
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     @ResponseStatus(HttpStatus.OK)
-    public UrlResponse uploadImage(@PathVariable Long id,
-                            @Parameter(required = true, description = "Image file")
-                            @RequestParam
-                            MultipartFile image){
-        return new UrlResponse(fileStoreService.uploadImage(id, image));
+    public UrlResponse uploadUserAvatar(@PathVariable Long id,
+                                        @Parameter(required = true, description = "Image file")
+                                   @RequestParam
+                                   MultipartFile image){
+        return new UrlResponse(fileStoreService.uploadUserAvatar(id, image));
+    }
+
+    @PostMapping(
+            path = "pets/{id}/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseStatus(HttpStatus.OK)
+    public UrlResponse uploadPetProfilesPhotos(@PathVariable Long id,
+                                        @Parameter(required = true, description = "Image file")
+                                        @RequestParam
+                                        List<MultipartFile> images){
+        return new UrlResponse(fileStoreService.uploadPetPhotos(id, images));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteImages(@PathVariable Long id,
+                             @RequestBody FileStoreOperation operation) {
+        fileStoreService.deleteImageByEntityId(id, operation);
     }
 }
