5a26324229769a18f9cfc5142a55ad6a0ad00b8d
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-05-18 22:08:11 +0300

    Create Location

---
+++ src/main/java/com/sniff/location/model/response/Location.java
@@ -0,0 +1,13 @@
+package com.sniff.location.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Location {
+    private Long id;
+    private String name;
+}
