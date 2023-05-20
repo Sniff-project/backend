57a6766818d23283d19eea83c009fbf208a88704
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-05-20 15:07:19 +0300

    Add password strength validation

--- src/main/java/com/sniff/utils/Validation.java
+++ src/main/java/com/sniff/utils/Validation.java
@@ -4,4 +4,8 @@ public class Validation {
     public static boolean isValidPhone(String phone) {
         return phone.matches("^\\+380\\d{9}$");
     }
+
+    public static boolean isValidPassword(String password) {
+        return password.matches("^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=])(?!.*\\s).{8,}$");
+    }
 }
