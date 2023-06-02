5a198213c387179ebfadf5bd630b813147c138e0
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-06-02 20:19:03 +0300

    Add query to get user pet cards

--- src/main/java/com/sniff/user/repository/UserRepository.java
+++ src/main/java/com/sniff/user/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.sniff.user.repository;
 
+import com.sniff.pet.enums.PetStatus;
+import com.sniff.pet.model.entity.Pet;
 import com.sniff.user.model.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -11,4 +16,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByPhone(String phone);
 
     Optional<User> findByEmailIgnoreCase(String email);
+
+    @Query("SELECT p FROM pet p " +
+            "WHERE (:status IS NULL OR p.status = :status) " +
+            "AND p.author.id = :id " +
+            "ORDER BY p.id DESC")
+    Page<Pet> getUserPetCards(Long id, PetStatus status, Pageable pageable);
 }
